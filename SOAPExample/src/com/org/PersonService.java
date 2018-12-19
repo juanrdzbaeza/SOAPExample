75897f9b6d99242a61bfe182eea8e9e878bfb3c1
juanrdzbaeza <juanrdzbaeza@gmail.com>
2018-12-19 13:44:48 +0100

    initial commit SOAPExample & SOAPExampleClient

---
+++ SOAPExample/src/com/org/PersonService.java
@@ -0,0 +1,14 @@
+package com.org;
+
+import com.org.Person;
+
+public interface PersonService {
+
+	public boolean addPerson(Person p);
+	
+	public boolean deletePerson(int id);
+	
+	public Person getPerson(int id);
+	
+	public Person[] getAllPersons();
+}
---
+++ SOAPExampleClient/src/com/org/PersonServiceImplProxy.java
@@ -0,0 +1,72 @@
+package com.org;
+
+public class PersonServiceImplProxy implements com.org.PersonServiceImpl {
+  private String _endpoint = null;
+  private com.org.PersonServiceImpl personServiceImpl = null;
+  
+  public PersonServiceImplProxy() {
+    _initPersonServiceImplProxy();
+  }
+  
+  public PersonServiceImplProxy(String endpoint) {
+    _endpoint = endpoint;
+    _initPersonServiceImplProxy();
+  }
+  
+  private void _initPersonServiceImplProxy() {
+    try {
+      personServiceImpl = (new com.org.PersonServiceImplServiceLocator()).getPersonServiceImpl();
+      if (personServiceImpl != null) {
+        if (_endpoint != null)
+          ((javax.xml.rpc.Stub)personServiceImpl)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+        else
+          _endpoint = (String)((javax.xml.rpc.Stub)personServiceImpl)._getProperty("javax.xml.rpc.service.endpoint.address");
+      }
+      
+    }
+    catch (javax.xml.rpc.ServiceException serviceException) {}
+  }
+  
+  public String getEndpoint() {
+    return _endpoint;
+  }
+  
+  public void setEndpoint(String endpoint) {
+    _endpoint = endpoint;
+    if (personServiceImpl != null)
+      ((javax.xml.rpc.Stub)personServiceImpl)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+    
+  }
+  
+  public com.org.PersonServiceImpl getPersonServiceImpl() {
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl;
+  }
+  
+  public boolean addPerson(com.org.Person p) throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.addPerson(p);
+  }
+  
+  public boolean deletePerson(int id) throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.deletePerson(id);
+  }
+  
+  public com.org.Person[] getAllPersons() throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.getAllPersons();
+  }
+  
+  public com.org.Person getPerson(int id) throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.getPerson(id);
+  }
+  
+  
+}
